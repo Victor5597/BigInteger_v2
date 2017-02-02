eb910d90d637680839cad4047e020782b3e53423
Victor <assassin5597@gmail.com>
2017-02-02 16:00:23 +0300

    -Added radix support for all operations -Added one-figure-numbers convertion to binary

--- BigInteger/Program.cs
+++ BigInteger/Program.cs
@@ -56,6 +56,38 @@ namespace BigInteger
     {
         public string s { get; private set; }
         public bool IsNeg { get; private set; }
+        public int Radix { get; private set; }
+        public BigInt(int num)
+        {
+            if (num < 0) IsNeg = true;
+            s = Reverse(Math.Abs(num).ToString());
+            s = RemoveZeros(s);
+        }
+        public BigInt(bool IsNegative, string str, bool IsReversed, int Rad = 10)
+        {
+            IsNeg = IsNegative;
+            if (IsReversed) s = str;
+            else s = Reverse(str);
+            s = RemoveZeros(s);
+            Radix = Rad;
+        }
+        public BigInt(string str, int Rad = 10)
+        {
+            if (str[0] == '-')
+            {
+                str = str.Remove(0, 1);
+                IsNeg = true;
+            }
+            else IsNeg = false;
+            Radix = Rad;
+            s = Reverse(str);
+            s = RemoveZeros(s);
+        }
+        public BigInt(BigInt n)
+        {
+            s = n.s;
+            IsNeg = n.IsNeg;
+        }
         public static string Reverse(string s)
         {
             char[] charArray = s.ToCharArray();
@@ -67,85 +99,32 @@ namespace BigInteger
             while (str.Last() == '0' && str.Length > 1) str = str.Remove(str.Length - 1);
             return str;
         }
-        public BigInt(int num)
+        
+        public static BigInt Sum(BigInt A, BigInt B)
         {
-            if (num < 0) IsNeg = true;
-            s = Reverse(Math.Abs(num).ToString());
-            s = RemoveZeros(s);
-        }
-        public BigInt(bool IsNegative, string str, bool IsReversed)
-        {
-            IsNeg = IsNegative;
-            if (IsReversed) s = str;
-            else s = Reverse(str);
-            s = RemoveZeros(s);
-        }
-        public BigInt(string str)
-        {
-            if (str[0] == '-')
+            string s = "";
+            BigInt a, b;
+            if (A > B)
             {
-                str = str.Remove(0, 1);
-                IsNeg = true;
-            }
-            else IsNeg = false;
-            s = Reverse(str);
-            s = RemoveZeros(s);
-        }
-        public BigInt(bool IsNegative, params int[] numbers)
-        {
-            IsNeg = IsNegative;
-            s = "";
-            for (int i = numbers.Count() - 1; i >= 0; i--)
-            {
-
-                s = s.Insert(0, numbers[i].ToString());
-
-            }
-            s = Reverse(s);
-            s = RemoveZeros(s);
-        }
-        public BigInt(bool IsNegative, params char[] figures)
-            : this(IsNegative, figures.ToString(), false) { }
-        public BigInt(BigInt n)
-        {
-            s = n.s;
-            IsNeg = n.IsNeg;
-        }
-        public static string Sum(string str1, string str2)
-        {
-            string a, b, s = "";
-            if (str1.Length < str2.Length)
-            {
-                a = str2;
-                b = str1;
+                a = A;
+                b = B;
             }
             else
             {
-                a = str1;
-                b = str2;
+                a = B;
+                b = A;
             }
-            a += '0';
-            b += new string('0', a.Length - b.Length);
+            a.s += '0';
+            b.s += new string('0', a.s.Length - b.s.Length);
             int cur = 0;
-            for (int i = 0; i < a.Length; i++)
+            for (int i = 0; i < a.s.Length; i++)
             {
-                cur = a[i] - '0' + b[i] - '0' + cur;
-                s += (char)('0' + cur % 10);
-                cur /= 10;
-                /*if (a[i] + b[i] + count < '5' * 2) //wtf?!
-                {
-                    s += (char)(a[i] + b[i] + count - '0');
-                    count = 0;
-                }
-                else
-                {
-                    s += (char)(a[i] + b[i] + count - '0' - 10);
-                    count = 1;
-                }*/
+                cur = a.s[i] - '0' + b.s[i] - '0' + cur;
+                s += (char)('0' + cur % A.Radix);
+                cur /= A.Radix;
             }
-            s = s.Insert(b.Length, a.Substring(b.Length));
             s = RemoveZeros(s);
-            return s;
+            return new BigInt(A.IsNeg, s, true, A.Radix);
         }
         public static BigInt Sub(BigInt A, BigInt B)
         {
@@ -175,7 +154,7 @@ namespace BigInteger
                 }
                 else
                 {
-                    s += (char)(a.s[i] - b.s[i] - count + '0' + 10);
+                    s += (char)(a.s[i] - b.s[i] - count + '0' + A.Radix);
                     count = 1;
                 }
             }
@@ -237,16 +216,15 @@ namespace BigInteger
         }
         public static BigInt operator +(BigInt A, BigInt B)
         {
-            string s;
+            if (A.Radix != B.Radix) throw new Exception();
             if (A.IsNeg == B.IsNeg)
             {
-                s = Sum(A.s, B.s);
+                return Sum(A, B);
             }
             else
             {
                 return Sub(A, B);
             }
-            return new BigInt(A.IsNeg, s, true);
         }
         public static BigInt operator +(BigInt A, int b)
         {
@@ -273,7 +251,7 @@ namespace BigInteger
         }
         public static BigInt operator *(BigInt A, double d)
         {
-            if (d == 10)
+            if (d == A.Radix)
             {
                 if (A.s == "0") return A;
                 A.s = A.s.Insert(0, "0");
@@ -283,6 +261,7 @@ namespace BigInteger
         }
         public static BigInt operator *(BigInt A, BigInt B)
         {
+            if (A.Radix != B.Radix) throw new Exception();
             Complex[] a, b;
             int N = 2 << (int)Math.Ceiling(Math.Log(Math.Max(A.s.Length, B.s.Length)) / Math.Log(2));
             a = A.FFT(N);
@@ -296,16 +275,17 @@ namespace BigInteger
             BigInt C = new BigInt(0);
             for (int i = 0; i < a.Length - 1; i++)
             {
-                C = C * 10 + new BigInt((int)Math.Round(a[i].Real));
+                C = C * A.Radix + new BigInt((int)Math.Round(a[i].Real));
             }
             return C;
         }
         public static BigInt operator /(BigInt N, BigInt D)
         {
+            if (N.Radix != D.Radix) throw new Exception();
             if (N < D) return new BigInt(0);
             int size = N.s.Length - D.s.Length + 1;
             int pow = D.s.Length;
-            BigInt x = new BigInt(10 / (D.s.Last() - '0'));
+            BigInt x = new BigInt(N.Radix / (D.s.Last() - '0'));
             x.s = new string('0', size) + x.s;
             BigInt two = new BigInt(2);
             two.s = new string('0', size + pow) + two.s;
@@ -331,6 +311,51 @@ namespace BigInteger
             if (IsNeg) System.Console.Write('-');
             System.Console.WriteLine(Reverse(s));
         }
+        public static BigInt DecToBin(BigInt a) {
+            if (a.s.Length == 1) return new BigInt(Table_DecToBin(a.s[0]), 2);
+            int N = 1 << (int)Math.Ceiling(Math.Log(a.s.Length) / Math.Log(2));
+            a.s += new string('0', N - a.s.Length);
+            BigInt a1 = new BigInt(a.s.Substring(0, a.s.Length / 2), 2);
+            BigInt a2 = new BigInt(a.s.Substring(a.s.Length / 2), 2);
+            //s = s1 + s2 * 10^length/2
+            a1 = DecToBin(a1);
+            a2 = DecToBin(a2);
+            return Merge(a1, a2, a.s.Length/2);
+        }
+        static BigInt Merge(BigInt a, BigInt B, int shift)
+        {
+            BigInt A = new BigInt(a.s.Substring(shift), 2);
+            a.s = a.s.Substring(0, shift);
+            return new BigInt(a.s + (B + A).s, 2);
+        }
+        static string Table_DecToBin(char c)
+        {
+            switch (c)
+            {
+                case '0':
+                    return "0";
+                case '1':
+                    return "1";
+                case '2':
+                    return "10";
+                case '3':
+                    return "11";
+                case '4':
+                    return "100";
+                case '5':
+                    return "101";
+                case '6':
+                    return "110";
+                case '7':
+                    return "111";
+                case '8':
+                    return "1000";
+                case '9':
+                    return "1001";
+                default:
+                    throw new Exception();
+            }
+        }
     }
     class Program
     {
@@ -339,8 +364,8 @@ namespace BigInteger
             while (true)
             {
                 BigInt a = new BigInt(Console.ReadLine());
-                BigInt b = new BigInt(Console.ReadLine());
-                a -= b;
+                //BigInt b = new BigInt(Console.ReadLine(), 2);
+                a = BigInt.DecToBin(a);
                 a.Print();
             }
 
