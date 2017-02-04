b425f79337ffb84175f590ea83d2dae14e5ec574
Victor <assassin5597@gmail.com>
2017-02-04 07:40:17 +0300

    ...

--- BigInteger/Program.cs
+++ BigInteger/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Numerics;
 
 namespace BigInteger
 {
@@ -16,14 +15,10 @@ namespace BigInteger
             l.Add(new BigInt(1));
             l.Add(new BigInt(5));
             l.Sort();
-            Matrix3x2 d = new Matrix3x2(1, 0, 0, 1, 1, 1);
-            Console.WriteLine(d.GetDeterminant());
             while (true)
             {
                 //Console.WriteLine(BigInt.DecToBin(new BigInt(Console.ReadLine(), 10)).ToString());
             }
-
-
         }
     }
 }
--- BigInteger/TimSort.cs
+++ BigInteger/TimSort.cs
@@ -8,5 +8,6 @@ namespace BigInteger
 {
     class TimSort
     {
+        
     }
 }
